832bd6509586aa415f9db0db97f403744ed377a5
xieyingliang <905756539@qq.com>
2018-07-30 12:06:18 +0800

    商品列表初步完成 价格无法正确显示。。

--- src/view/commodityList/commodityList.vue
+++ src/view/commodityList/commodityList.vue
@@ -1,192 +1,200 @@
 <template>
 
-    <div>
-       <!--<el-row style="padding-bottom: 20px;">
-            商品列表
-        </el-row>-->
-        <el-container direction="vertical">
-            <fm-grid url="/wgoods/list" ref="grid" method="get" :params="['kw','brandId','categoryId','status']">
+  <div>
+    <!--<el-row style="padding-bottom: 20px;">
+         商品列表
+     </el-row>-->
+    <el-container direction="vertical">
+      <fm-grid url="/wgoods/list" ref="grid" method="get" :params="['kw','brandId','categoryId','status']">
 
 
-                <template slot-scope="{rows,loading,search}">
+        <template slot-scope="{rows,loading,search}">
 
-                    <div class="filter-container">
-                        <!--<el-input style="width: 200px;" size="small" v-model="query.kw" class="filter-item"-->
-                        <!--placeholder="请输入姓名或用户名"/>-->
+          <div class="filter-container">
+            <!--<el-input style="width: 200px;" size="small" v-model="query.kw" class="filter-item"-->
+            <!--placeholder="请输入姓名或用户名"/>-->
 
-                        <!--<el-button class="filter-item" size="small" style="margin-left: 10px;" @click="search(query,1)"-->
-                        <!--type="primary" icon="search">搜索-->
-                        <!--</el-button>-->
-                        <!--<div style="float:right;margin-right:20px;">-->
-                        <!--<el-button type="text" icon="document" @click="">-->
-                        <!--添加-->
-                        <!--</el-button>-->
-                        <!--</div>-->
-                        <!--<el-row style="padding-bottom: 20px;">
-                            <el-button @click="gSearch('all')">全部商品{{count.all}}</el-button>
-                            <el-button @click="gSearch('wcm')">待审核{{count.wcm}}</el-button>
-                            <el-button @click="gSearch('upr')">已上架{{count.upr}}</el-button>
-                            <el-button @click="gSearch('lwr')">已下架{{count.lwr}}</el-button>
-                        </el-row>-->
-                        <el-row style="padding-bottom: 20px;">
-                            <span>输入搜索:</span>
-                            <el-input v-model="query.kw"  placeholder="商品名称/商品货号" style="width:200px;"/>
-                            <!--<span>商品分类:</span>
-                            <el-cascader
-                                expand-trigger="hover"
-                                :options="categories"
-                                :props="{value:'id',label:'name'}"
-                                v-model="query.categoryId"
-                                @change="categoryChange">
-                            </el-cascader>
-                            <span>商品品牌:</span>
-                            <el-select v-model="query.brandId" placeholder="请选择">
-                                <el-option
-                                    v-for="item in brands"
-                                    :key="item.id"
-                                    :label="item.name"
-                                    :value="item.id">
-                                </el-option>
-                            </el-select>-->
-                            <el-button @click="search(query,1)">查询结果</el-button>
-                        </el-row>
+            <!--<el-button class="filter-item" size="small" style="margin-left: 10px;" @click="search(query,1)"-->
+            <!--type="primary" icon="search">搜索-->
+            <!--</el-button>-->
+            <!--<div style="float:right;margin-right:20px;">-->
+            <!--<el-button type="text" icon="document" @click="">-->
+            <!--添加-->
+            <!--</el-button>-->
+            <!--</div>-->
+            <!--<el-row style="padding-bottom: 20px;">
+                <el-button @click="gSearch('all')">全部商品{{count.all}}</el-button>
+                <el-button @click="gSearch('wcm')">待审核{{count.wcm}}</el-button>
+                <el-button @click="gSearch('upr')">已上架{{count.upr}}</el-button>
+                <el-button @click="gSearch('lwr')">已下架{{count.lwr}}</el-button>
+            </el-row>-->
+            <el-row style="padding-bottom: 20px;">
+              <span>输入搜索:</span>
+              <el-input v-model="query.kw" placeholder="商品名称/商品货号" style="width:200px;"/>
+              <!--<span>商品分类:</span>
+              <el-cascader
+                  expand-trigger="hover"
+                  :options="categories"
+                  :props="{value:'id',label:'name'}"
+                  v-model="query.categoryId"
+                  @change="categoryChange">
+              </el-cascader>
+              <span>商品品牌:</span>
+              <el-select v-model="query.brandId" placeholder="请选择">
+                  <el-option
+                      v-for="item in brands"
+                      :key="item.id"
+                      :label="item.name"
+                      :value="item.id">
+                  </el-option>
+              </el-select>-->
+              <el-button @click="search(query,1)">查询结果</el-button>
+            </el-row>
 
-                    </div>
-                    <el-table
-                        :data="rows"
-                        stripe
-                        v-loading="loading"
-                        style="width: 100%;padding-bottom:20px;border-bottom:none;">
-                        <el-table-column
-                            prop="id"
-                            label="编号"
-                            width="100">
-                        </el-table-column>
-                        <el-table-column
-                            prop="mainImg"
-                            label="商品图片"
-                            width="120">
-                            <template slot-scope="scope">
-                                <img :src="scope.row.goodsImg" alt="" style="width: 50px;height: 50px">
-                            </template>
-                        </el-table-column>
-                        <el-table-column
-                            prop="goodsName"
-                            label="商品名称"
-                            width="180">
-                        </el-table-column>
-                         <el-table-column
-                            prop="price"
-                            label="价格"
-                            width="180">
-                          </el-table-column>
-                        <el-table-column
-                           prop="status"
-                            label="状态"
-                            width="100">
-                              <template slot-scope="scope">
-                                  <span v-if="scope.row.status===0">待审核 </span>
-                                   <span v-else-if="scope.row.status===1">在售 </span>
-                                   <span v-else>已下架 </span>
-                                </template>
-                        </el-table-column>
-                        <el-table-column
-                            prop="saleAmont"
-                            label="销量"
-                            width="100">
-                        </el-table-column>
-                        <el-table-column
-                            prop="categoryInfo.name"
-                            label="商品分类"
-                            width="100">
-                        </el-table-column>
-                        <el-table-column
-                            label="操作">
-                            <template slot-scope="{row}">
-                                <el-button type="text" @click="">编辑</el-button>
-                                <el-button type="text" @click="upper(row.id)">上架</el-button>
-                                <el-button type="text" @click="lower(row.id)">下架</el-button>
-                                <el-button type="text" @click="del(row.id)">删除</el-button>
-                            </template>
-                        </el-table-column>
-                    </el-table>
-                </template>
+          </div>
+          <el-table
+            :data="rows"
+            stripe
+            v-loading="loading"
+            style="width: 100%;padding-bottom:20px;border-bottom:none;">
+            <el-table-column
+              prop="id"
+              label="编号"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              prop="mainImg"
+              label="商品图片"
+              width="120">
+              <template slot-scope="scope">
+                <img :src="scope.row.goodsImg" alt="" style="width: 50px;height: 50px">
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="goodsName"
+              label="商品名称"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="price"
+              label="价格"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="status"
+              label="状态"
+              width="100">
+              <template slot-scope="scope">
+                <span v-if="scope.row.status===0">待审核 </span>
+                <span v-else-if="scope.row.status===1">在售 </span>
+                <span v-else>已下架 </span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="saleAmont"
+              label="销量"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              prop="categoryInfo.name"
+              label="商品分类"
+              width="100">
+            </el-table-column>
+            <el-table-column
+              label="操作">
+              <template slot-scope="{row}">
+                <el-button type="text" @click="review(row.id,1)">审核通过</el-button>
+                <el-button type="text" @click="upper(row.id)">上架</el-button>
+                <el-button type="text" @click="lower(row.id)">下架</el-button>
+                <el-button type="text" @click="del(row.id)">删除</el-button>
+                <el-button type="text" @click="review(row.id,0)">审核不通过</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </template>
 
-            </fm-grid>
-            <!--<dict-ae @success="$refs.grid.search()" ref="dictae"/>-->
-        </el-container>
+      </fm-grid>
+      <goodsEdit @success="$refs.grid.search()" ref="dictae"/>
+    </el-container>
 
-    </div>
+  </div>
 
 
 </template>
 
 <script>
+  import goodsEdit from './goodsEdit';
 
-    export default {
+  export default {
+    components: {goodsEdit},
+    mounted() {
+      this.$nextTick(() => {
+        const {kw} = this.$route.query;
+        this.query = {...this.query, kw};
+        this.$shop.getCategories().then(categories => this.categories = categories);
+        this.$shop.getBrands().then(brands => this.brands = brands);
+        this.loadCount();
+      });
+    },
 
-        mounted() {
-            this.$nextTick(() => {
-                const {kw} = this.$route.query;
-                this.query = {...this.query, kw};
-                this.$shop.getCategories().then(categories =>  this.categories = categories);
-                this.$shop.getBrands().then(brands =>  this.brands = brands);
-                this.loadCount();
-            });
-        },
+    methods: {
 
-        methods: {
+      loadCount() {
+        this.$axios.get('/wgoods/allCount')
+          .then(({data: {data}}) => {
+            this.count = data;
+          });
+      },
 
-            loadCount(){
-                this.$axios.get('/wgoods/allCount')
-                    .then(({data: {data}}) => {
-                        this.count = data;
-                    });
-            },
+      upper(id) {
+        this.$axios.post('/wgoods/upper', this.$axios.form({goodsId: id}))
+          .then(({data}) => {
+            this.$refs.grid.search();
+            this.loadCount();
+          });
+      },
 
-            upper(id){
-                this.$axios.post('/wgoods/upper',this.$axios.form({goodsId:id}))
-                    .then(({data}) =>{
-                        this.$refs.grid.search();
-                        this.loadCount();
-                    });
-            },
+      lower(id) {
+        this.$axios.post('/wgoods/lower', this.$axios.form({goodsId: id}))
+          .then(({data}) => {
+            this.$refs.grid.search();
+            this.loadCount();
+          });
+      },
 
-            lower(id){
-                this.$axios.post('/wgoods/lower',this.$axios.form({goodsId:id}))
-                    .then(({data}) =>{
-                        this.$refs.grid.search();
-                        this.loadCount();
-                    });
-            },
+      del(id) {
+        this.$axios.post('/wgoods/del', this.$axios.form({goodsId: id}))
+          .then(({data}) => {
+            this.$refs.grid.search();
+            this.loadCount();
+          });
+      },
+      review(id,flag) {
+        this.$axios.post('/wgoods/review', this.$axios.form({goodsId: id,flag:flag}))
+          .then(({data}) => {
+            this.$refs.grid.search();
+            this.loadCount();
+          });
+      },
+      categoryChange() {
 
-            del(id){
-                this.$axios.post('/wgoods/del',this.$axios.form({goodsId:id}))
-                    .then(({data}) =>{
-                        this.$refs.grid.search();
-                        this.loadCount();
-                    });
-            },
+      },
 
-            categoryChange() {
+      gSearch(status) {
+        this.$refs.grid.search({...this.query, status}, 1);
+      }
 
-            },
+    },
 
-            gSearch(status){
-                this.$refs.grid.search({...this.query,status},1);
-            }
-
-        },
-
-        data() {
-            return {
-                query: {},
-                categories: [],
-                brands: [],
-                count:{},
-            }
-        }
+    data() {
+      return {
+        query: {},
+        categories: [],
+        brands: [],
+        count: {},
+      }
     }
+  }
 
 
 </script>
