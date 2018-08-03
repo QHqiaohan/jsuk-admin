91f7aa82df4a7480eed22a6dd486b1e6d29ad4c4
xieyingliang <905756539@qq.com>
2018-08-03 13:46:52 +0800

    配送管理

--- src/view/banner/banner.vue
+++ src/view/banner/banner.vue
@@ -1,13 +1,43 @@
 <template>
-    
+  <div>
+    <div>首页banner</div>
+    <div>
+      <div>管理banner图片</div>
+      <div>
+        <ul style="display: block">
+          <li v-for="item in items">
+            <img :src="item.img" style="width: 100px;height: 180px;"/>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "banner"
+  export default {
+    data() {
+      return {
+        query: {},
+        categories: [],
+        brands: [],
+        count: {},
+        items: [{img: 'https://jsuke.oss-cn-shenzhen.aliyuncs.com/image/1528950854236501094.png'},
+          {img: 'https://jsuke.oss-cn-shenzhen.aliyuncs.com/image/1528950854236501094.png'},
+          {img: 'https://jsuke.oss-cn-shenzhen.aliyuncs.com/image/1528950854236501094.png'},
+          {img: 'https://jsuke.oss-cn-shenzhen.aliyuncs.com/image/1528950854236501094.png'}],
+      }
     }
+  }
 </script>
 
 <style scoped>
+  li {
+    text-decoration: none; /*去掉前面的圆点*/
+    list-style: none;
+    float: left;
+    border: 1px solid #FFFFFF;
+    background-color: #30DDEB;
+  }
 
 </style>
