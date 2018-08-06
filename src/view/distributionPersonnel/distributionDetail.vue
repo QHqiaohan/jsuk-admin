10793116674f1730a4f4300fa13ffdd210091194
xieyingliang <905756539@qq.com>
2018-08-06 14:56:15 +0800

    配送管理 - 用户详情(未分页)。。

--- src/view/distributionPersonnel/distributionDetail.vue
+++ src/view/distributionPersonnel/distributionDetail.vue
@@ -2,50 +2,43 @@
 
   <!--"/distribution/orderDetails?id=" +id-->
   <el-dialog :title="'用户详情' " :visible="visible" @close="visible = false" width="70%">
-    <el-container direction="vertical">
-      <fm-grid v-bind="fm" ref="grid" method="get" :params="['id']">
-        <template slot-scope="{rows,loading,search}">
-          <el-table
-            :data="rows"
-            stripe
-            v-loading="loading"
-            style="width: 100%;padding-bottom:20px;border-bottom:none;">
-            <el-table-column
-              prop="orderNo"
-              label="订单编号"
-              width="200">
-            </el-table-column>
-            <el-table-column
-              prop="publishTime"
-              label="提交时间"
-              width="220">
-            </el-table-column>
-            <el-table-column
-              prop="user.phone"
-              label="用户账号"
-              width="200">
-            </el-table-column>
-            <el-table-column
-              prop="price"
-              label="订单金额"
-              width="200">
-            </el-table-column>
-            <el-table-column
-              prop="status"
-              label="订单状态"
-              width="180">
-              <template slot-scope="scope">
-                <span v-if="scope.row.status===0">取消 </span>
-                <span v-else-if="0<scope.row.status<=3">待取货 </span>
-                <span v-else-if="3<scope.row.status<5">已配送 </span>
-                <span v-else>已完成 </span>
-              </template>
-            </el-table-column>
-          </el-table>
+    <el-table
+      :data="sizes"
+      stripe
+      v-loading="loading"
+      style="width: 100%;padding-bottom:20px;border-bottom:none;">
+      <el-table-column
+        prop="orderNo"
+        label="订单编号"
+        width="200">
+      </el-table-column>
+      <el-table-column
+        prop="publishTime"
+        label="提交时间"
+        width="220">
+      </el-table-column>
+      <el-table-column
+        prop="user.phone"
+        label="用户账号"
+        width="200">
+      </el-table-column>
+      <el-table-column
+        prop="price"
+        label="订单金额"
+        width="200">
+      </el-table-column>
+      <el-table-column
+        prop="status"
+        label="订单状态"
+        width="180">
+        <template slot-scope="scope">
+          <span v-if="scope.row.status===0">取消 </span>
+          <span v-else-if="0<scope.row.status<=3">待取货 </span>
+          <span v-else-if="3<scope.row.status<5">已配送 </span>
+          <span v-else>已完成 </span>
         </template>
-
-      </fm-grid>
-    </el-container>
+      </el-table-column>
+    </el-table>
   </el-dialog>
 
 </template>
@@ -67,15 +60,23 @@
         visible: false,
         fm: {},
         form: form(),
+        sizes: []
       }
     },
 
     methods: {
       view(id) {
-        this.visible = true;
+        /*this.visible = true;
         this.fm.url = "/distribution/orderDetails?id=" + id;
         this.fm.current = 1;
-        this.$refs.grid.search();
+        this.$refs.grid.search();*/
+        this.visible = true;
+        this.$axios.get(`/distribution/orderDetails`, {params: {id}})
+          .then(({data: {data}}) => {
+            console.log(data);
+            this.sizes = data.records;
+            this.form = data;
+          });
       },
       edit(id) {
         this.visible = true;
